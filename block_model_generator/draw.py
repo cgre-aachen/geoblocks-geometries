2479d0f0e9141b5dea1197c511b98835955ffadf
yangjiandendi <yangjian@web.de>
2023-10-30 13:39:35 +0100

    fix bugs

--- block_model_generator/draw.py
+++ block_model_generator/draw.py
@@ -1,9 +1,14 @@
 import pygame
 import sys
 import json
+import tkinter.filedialog
+from PyQt5.QtWidgets import QApplication, QFileDialog
+
+
 
 # Initialize pygame
 pygame.init()
+
 
 # Set up display
 width, height = 810, 610
@@ -18,6 +23,8 @@ BLACK = (0, 0, 0)
 # Variables
 drawing = False
 points = []  # List to store points of the curve
+background_image = None
+
 
 # Set the frame rate (frames per second)
 FPS = 15
@@ -25,6 +32,7 @@ clock = pygame.time.Clock()
 
 # Main game loop
 while True:
+    window.fill(WHITE)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             # Save points to a JSON file before exiting
@@ -33,14 +41,26 @@ while True:
             pygame.quit()
             sys.exit()
 
-        # Check for mouse events
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:  # Left mouse button pressed
-                drawing = True
-                points.append([])  # Start a new segment of the curve
+                if upload_button_rect.collidepoint(event.pos):
+                    drawing = False
+                    # Open a file dialog for image selection
+                    app = QApplication(sys.argv)
+                    file_dialog = QFileDialog.getOpenFileName()[0]
+                    if file_dialog:  # If a file is selected
+                        background_image = pygame.image.load(file_dialog)
+                        background_image = pygame.transform.scale(background_image, (width, height))
+
+                elif upload_button_rect.collidepoint(event.pos) == False:                      
+                    drawing = True
+                    points.append([])  # Start a new segment of the curve
         elif event.type == pygame.MOUSEBUTTONUP:
             if event.button == 1:  # Left mouse button released
                 drawing = False
+    # Draw the background image if available
+    if background_image:
+        window.blit(background_image, (0, 0))            
 
     # Capture points every frame if drawing is True
     if drawing:
@@ -48,7 +68,9 @@ while True:
         if points:  # Ensure there is at least one segment
             points[-1].append((x, y))  # Add points to the last segment of the curve
 
-    window.fill(WHITE)
+ 
+
+
     # Draw grid lines
     GRID_SIZE = 40  # Size of each grid cell
     TICK_SIZE = 10  # Size of the ticks on the axes
@@ -69,10 +91,18 @@ while True:
             window.blit(label, (TICK_SIZE, y - label.get_height() // 2))
 
 
+    # Draw the upload button
+    upload_button_text = "Upload Image"
+    upload_button_font = pygame.freetype.Font(None, 24)
+    upload_button_surface, upload_button_rect = upload_button_font.render(upload_button_text, BLACK)
+    upload_button_rect.topleft = (20, 20)
+    window.blit(upload_button_surface, upload_button_rect)
+
+
     # Draw the curve
     for segment in points:
         if len(segment) > 1:
-            pygame.draw.lines(window, BLACK, False, segment, 2)
+            pygame.draw.lines(window, BLACK, False, segment, 6)
 
     pygame.display.flip()
 
